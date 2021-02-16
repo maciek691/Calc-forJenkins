f6a88cd0fa1300d292c3d2d9e88da04992325c8a
Maciek Wachowski <maciekmaciek691@gmail.com>
2021-02-16 09:12:25 +0100

    code clean

--- src/main/java/CalcMainMethods.java
+++ src/main/java/CalcMainMethods.java
@@ -1,29 +1,29 @@
-import java.util.Scanner;
-
 public class CalcMainMethods {
 
-    public double add (double numb1, double numb2){ return numb1 + numb2; }
+    public double add(double numb1, double numb2) {
+        return numb1 + numb2;
+    }
 
-    public double subtract (double numb1, double numb2) {
+    public double subtract(double numb1, double numb2) {
         return numb1 - numb2;
     }
 
-    public double multiply (double numb1, double numb2) {
+    public double multiply(double numb1, double numb2) {
         return numb1 * numb2;
     }
 
-    public double divide (double numb1, double numb2) {
-        if(numb2==0){
+    public double divide(double numb1, double numb2) {
+        if (numb2 == 0) {
             throw new ArithmeticException("Nie można dzielić przez 0. Wybierz 1 i ustaw nowe wartości liczb.");
         }
         return numb1 / numb2;
     }
 
-    public double power (double numb1, double numb2) {
+    public double power(double numb1, double numb2) {
         return Math.pow(numb1, numb2);
     }
 
-    public double root (double numb1, double numb2) {
-        return Math.pow(numb1,1/numb2);
+    public double root(double numb1, double numb2) {
+        return Math.pow(numb1, 1 / numb2);
     }
 }
